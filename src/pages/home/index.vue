dd3c331dbfd8c24f8992b7515c5b7be6dc087450
weirdo_world <123456>
2021-02-07 15:54:52 +0800

    产品布局调整

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -8,7 +8,13 @@
       </div>
     </swiper>
     <div class="shop-body">
-      <p class="shop-title">产品展示</p>
+      <div class="shop-title">店长推荐</div>
+      <div class="product" v-for="item in productImg">
+        <img :src="item.url" mode="aspectFit">
+        <p>名称：<span>{{ item.title }}</span></p>
+        <p>价格：<span>{{ item.pric }}</span></p>
+      </div>
+      <div class="shop-title">热销产品</div>
       <div class="product" v-for="item in productImg">
         <img :src="item.url" mode="aspectFit">
         <p>名称：<span>{{ item.title }}</span></p>
@@ -26,8 +32,7 @@ export default {
         {url: '/static/images/shop/01.jpg', title: '名称一', pric: '￥666'},
         {url: '/static/images/shop/02.jpg', title: '名称二', pric: '￥999'},
         {url: '/static/images/shop/03.jpg', title: '名称三', pric: '￥888'},
-        {url: '/static/images/shop/04.jpg', title: '名称四', pric: '￥699'},
-        {url: '/static/images/shop/05.jpg', title: '名称五', pric: '￥12000'}
+        {url: '/static/images/shop/04.jpg', title: '名称四', pric: '￥699'}
       ]
     }
   },
@@ -60,21 +65,22 @@ export default {
 
 .shop-body {
   width: 100%;
-  padding-bottom: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 }
 
 .shop-body .shop-title {
   width: 100%;
-  height: 30px;
   text-align: center;
   letter-spacing: 5px;
-  font-size: 24px;
+  font-size: 18px;
   font-weight: bold;
 
 }
 
 .shop-body .product {
-  width: 40%;
+  width: 42%;
   float: left;
   margin: 10px;
   padding: 0 5px;
